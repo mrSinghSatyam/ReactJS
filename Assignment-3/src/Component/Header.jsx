714c2b2c9add091917cf088fd1164c2502163bd4
mrSinghSatyam <satyamsinghrs972518@gmail.com>
2025-02-11 14:05:05 +0530

    2203031080232 - Assignment 33

---
+++ Assignment-3/src/Component/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <h1>"Welcome to My Portfolio"</h1>
+    <nav>
+        <ul>
+            <a href="#"><li>Home</li></a>
+            <a href="#"><li>About</li></a>
+            <a href=""><li>Contact</li></a>
+        </ul>
+    </nav>
+    </>
+  )
+}
+
+export default Header
